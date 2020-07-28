5c366e6e3eb0b4d5751775c69ab032c01d7befcf
Diana Arreola <arreoladiana11@gmail.com>
2020-07-28 03:27:03 -0700

    retrieved daily reading for each sign by web scraping horoscope.com 🔮

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import datetime, date
 from bisect import bisect
+from bs4 import BeautifulSoup
+from selenium_driver import driver
+import json
 
 
 def get_sun_sign(user_birth_date):
@@ -57,3 +60,57 @@ def check_day(month_num, day_num):
   except ValueError:
     print('The cosmos 🔮 cannot forsee your birth day.')
     return False
+
+
+def get_stored_daily_readings(sign, current_date):
+  # check if daily reading exists for a sign
+  with open('daily_readings.json') as json_file:
+    saved_readings = json.load(json_file)
+    if current_date in saved_readings[sign]:
+        return saved_readings[sign][current_date]
+    else:
+      return False
+
+
+def store_daily_readings(sign, current_date, horoscope):
+  # stores daily readings
+  # handle exceptions
+  with open('daily_readings.json', 'r+') as json_file:
+    saved_readings = json.load(json_file)
+    saved_readings[sign].update({current_date: horoscope})
+    json_file.seek(0)
+    json.dump(saved_readings, json_file, indent=2)
+    json_file.truncate()
+
+
+def get_daily_reading(user_command):
+  # horoscope.com's order of the zodiac sign
+  sign_order = {'aries': '1', 'taurus': '2', 'gemini': '3', 'cancer': '4', 'leo': '5',
+                'virgo': '6', 'libra': '7', 'scorpio': '8', 'sagittarius': '9',
+                'capricorn': '10', 'aquarius': '11', 'pisces': '12'}
+
+  sign = user_command.split(' ')[0].lower()
+  current_date = str(date.today())
+
+  # check if daily reading already exists
+  if get_stored_daily_readings(sign, current_date):
+    stored_reading = get_stored_daily_readings(sign, current_date)
+    return stored_reading
+
+  # web scraping daily reading from horoscope.com
+  url = 'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign='
+  sign_url = url + sign_order[sign]
+  driver.get(sign_url)
+  soup = BeautifulSoup(driver.page_source, "lxml")
+  driver.close()
+  p_tags = soup.find(class_='main-horoscope').find_all('p')
+  # may need to change
+  horoscope = str(p_tags[0])[35:-4]
+
+  # store daily reading in json file
+  store_daily_readings(sign, current_date, horoscope)
+
+  return horoscope
+
+
+get_daily_reading('Pisces daily reading')
